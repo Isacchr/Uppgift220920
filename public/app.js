189826eddca6ce50a3f6ff4217e6751d04477933
isacchr <isac.christiansson@gmail.com>
2022-09-22 12:16:43 +0200

    Hide/show button added for customer list

--- public/app.js
+++ public/app.js
@@ -8,7 +8,21 @@ var orderCustomerInfo = {
     Email: ''
 };
 
-function submitInfo() {
+build = () => {
+
+    const divInput = document.getElementById('divInput');
+    const divCustomerList = document.getElementById('customerList');
+
+    const hideShowBtn = document.createElement('button');
+    hideShowBtn.onclick = () => divCustomerList.hidden = true; 
+    hideShowBtn.innerText = 'Hide/Show Customer List';
+
+    hideShowBtn.onclick = () => divCustomerList.hidden = !divCustomerList.hidden;
+    divInput.appendChild(hideShowBtn);
+
+}
+
+submitInfo = () => {
 
     var customerName = document.getElementById('customerName').value;
     var customerTelephoneNr = document.getElementById('customerTelephoneNr').value;
@@ -24,5 +38,5 @@ function submitInfo() {
     document.getElementById('customerTelephoneNr').value = '';
     document.getElementById('customerAdress').value = '';
     document.getElementById('customerEmail').value = '';
-    
-    }
+
+}
