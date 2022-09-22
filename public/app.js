75bc7080b453f260098ad70438d26220f4847d36
isacchr <isac.christiansson@gmail.com>
2022-09-22 11:47:53 +0200

    inputs added and information stored in a array

--- public/app.js
+++ public/app.js
@@ -1,1 +1,23 @@
-console.log('hi from js');
+
+var customerInfo = [];
+
+var orderCustomerInfo = {
+    Name: '',
+    PhoneNumber: '',
+    Adress: '',
+    Email: ''
+};
+
+function submitInfo() {
+
+    var customerName = document.getElementById('customerName').value;
+    var customerTelephoneNr = document.getElementById('customerTelephoneNr').value;
+    var customerAdress = document.getElementById('customerAdress').value;
+    var customerEmail = document.getElementById('customerEmail').value;
+
+    var orderCustomerInfo = {Name: customerName, PhoneNumber: customerTelephoneNr, Adress: customerAdress, Email: customerEmail};
+    customerInfo.push(orderCustomerInfo);
+
+    console.log(customerInfo);
+
+}
